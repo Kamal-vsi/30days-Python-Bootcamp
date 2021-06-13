0c9e9e7d686f703961a90530454bc77949268845
K.Kamalakannan <85786346+Kamal-vsi@users.noreply.github.com>
2021-06-13 15:23:10 +0530

    Add files via upload

---
+++ Assignment.py.py
@@ -0,0 +1,26 @@
+#assignment 1
+
+# print hello world
+print ('Hello World')
+print("by K.Kamalakannan")
+
+#add two numbers
+a=10
+b=25
+c=a+b
+print(c)
+
+# find maximum
+a=int(input("enter numb1: "))
+b=int(input("enter numb2: "))
+if(a>b):
+    print("num1 is maximum")
+else:
+    print("num2 is maximum")
+
+#  OR 
+a=int(input("enter num1:"))
+b=int(input("enter num2:"))
+c=max(a,b)
+print("maximum value is:")
+print(c)
