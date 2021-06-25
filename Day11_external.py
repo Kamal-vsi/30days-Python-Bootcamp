a9ee05c525c25aade8b20fe80d38ed1b0e7cacfe
K.Kamalakannan <85786346+Kamal-vsi@users.noreply.github.com>
2021-06-25 16:25:48 +0530

    Add files via upload

---
+++ Day11_external.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+list1=['kamal1', 'sriram2', 'trump3']
+def ext_func(list1):
+    print("Let's say hello to everyone!")
+    for i in list1:
+        print("hello",i)
+
